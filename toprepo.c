9182ad3f208b2bd0ee696c3331c7bc2ffb37a680
tomsandholm <tom.sandholm@gmail.com>
2021-03-21 11:03:28 -0400

    fix

---
+++ toprepo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char ** argv) {
+  printf("hello from suba\n");
+  system("suba");
+  printf("hello from subb\n");
+  system("subb");
+  printf("hello from subc\n");
+  system("subc");
+  return 0;
+}
+
